d513465d1c9a0ac91b20bc80d441430f9596b58b
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-05-13 02:36:56 +0600

    server created and set up completed

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.CAREER_PASS) //
+
+//MONGODB
+const uri = `mongodb+srv://${process.env.CAREER_USER}:${process.env.CAREER_PASS}@cluster0.sirqfba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const jobCollection = client.db('careerDB').collection('job');
+
+    // app.get('/job',async(req,res)=>{
+    //     const cursor = jobCollection.find();
+    //     const result = await cursor.toArray();
+    //     res.send(result);
+    // })
+    // app.get('/job/:id',async(req,res)=>{
+    //     const id = req.params.id;
+    //     const query = {_id: new ObjectId(id)}
+    //     const result = await jobCollection.findOne(query);
+    //     res.send(result);
+    // })
+
+    //add A new Job
+    app.post('/job',async(req,res)=>{
+        const newJob = req.body;
+        console.log(newJob);
+        const result = await jobCollection.insertOne(newJob);
+        res.send(result);
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send('Career Bridge Server is running')
+})
+app.listen(port,()=>{
+    console.log(`Career Bridge Server is running on port ${port}`)
+})
